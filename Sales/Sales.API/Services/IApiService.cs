5eeec27528601f686dd419c2de209da054610606
guillermocarrillo <guillermocarrillo1120106@correo.itm.edu.co>
2023-05-05 18:16:23 -0500

    Consume external API of Countries, States and Cities

---
+++ Sales/Sales.API/Services/IApiService.cs
@@ -0,0 +1,10 @@
+﻿using Sales.Shared.Responses;
+using Sales.Shared.Responses.Sales.Shared.Responses;
+
+namespace Sales.API.Services
+{
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string servicePrefix, string controller);
+    }
+}
---
+++ Sales/Sales.Shared/Responses/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Sales.Shared.Responses
+{
+    namespace Sales.Shared.Responses
+    {
+        public class Response
+        {
+            public bool IsSuccess { get; set; }
+
+            public string? Message { get; set; }
+
+            public object? Result { get; set; }
+        }
+    }
+}
